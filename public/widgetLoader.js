5e3580cc5221119aa72a3d085769cb665e3de5a5
caspar wikstrom <caspar.wikstrom@gmail.com>
2023-05-12 13:44:06 +0200

    ddd

--- public/widgetLoader.js
+++ public/widgetLoader.js
@@ -4,7 +4,7 @@ yourAppWidget.render = function (containerId, settings) {
   var iframe = document.createElement('iframe');
   iframe.width = '100%';
   iframe.height = '500';
-  iframe.src = 'https://your-nextjs-app.com/your-component?apiKey=' + encodeURIComponent(settings.apiKey);
+  iframe.src = `https://widgets-theta-one.vercel.app/cardwidget?widgetId=${settings.widgetId}`;
 
   var container = document.getElementById(containerId);
   container.innerHTML = '';
